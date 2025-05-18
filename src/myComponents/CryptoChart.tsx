8384b0425d4363560ac9579e499264c7271ff9f0
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-05-18 08:29:14 +0530

    graph color change

--- src/myComponents/CryptoChart.tsx
+++ src/myComponents/CryptoChart.tsx
@@ -164,8 +164,8 @@ useEffect(() => {
                             >
                                 <defs>
                                     <linearGradient id="colorPrice" x1="0" y1="0" x2="0" y2="1">
-                                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                                        <stop offset="5%" stopColor="#FFD700" stopOpacity={0.8} />
+                                        <stop offset="95%" stopColor="#FFD700" stopOpacity={0} />
                                     </linearGradient>
                                 </defs>
                                 <CartesianGrid strokeDasharray="3 3" opacity={0.2} />
